a1fad89be5a9d8d7270380dda60b5c6c9ee29372
redi2s <redi2s@ya.ru>
2022-04-11 23:21:34 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
+from phonenumber_field.phonenumber import PhoneNumber
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -63,23 +64,51 @@ def product_list_api(request):
 def register_order(request):
     new_order = request.data
 
-    if 'products' not in new_order:
-        return Response({'error': 'Key \'products\' must be presented'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    required_keys = ['products', 'firstname', 'lastname', 'phonenumber', 'address']
+    for key in required_keys:
+        if key not in new_order:
+            return Response({'error': f'Key \'{key}\' must be presented'}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
-    if not isinstance(new_order['products'], list):
-        return Response({'error': 'Product key is not type of list'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    for key, value in new_order.items():
+        if not value:
+            return Response({'error': f'The field of key \'{key}\' cannot be empty or null'},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
 
-    if not new_order['products']:
-        return Response({'error': 'List of products must be presented'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    new_order_address = new_order['address']
+    new_order_firstname = new_order['firstname']
+    new_order_lastname = new_order['lastname']
+    new_order_phonenumber = new_order['phonenumber']
+    new_order_products = new_order['products']
+
+    if not isinstance(new_order_products, list):
+        return Response({'error': 'The key \'products\' is not type of list'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    if not isinstance(new_order_firstname, str):
+        return Response({'error': 'The key \'firstname\' is not type of str'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    if not isinstance(new_order_lastname, str):
+        return Response({'error': 'The key \'lastname\' is not type of str'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    if not isinstance(new_order_phonenumber, str):
+        return Response({'error': 'The key \'phonenumber\' is not type of str'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    if not isinstance(new_order_address, str):
+        return Response({'error': 'The key \'address\' is not type of str'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    is_phonenumber_valid = PhoneNumber.from_string(phone_number=new_order_phonenumber).is_valid()
+    if not is_phonenumber_valid:
+        return Response({'error': 'Phonenumber is not valid'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    for new_order_product in new_order_products:
+        try:
+            Product.objects.get(pk=new_order_product['product'])
+        except Product.DoesNotExist:
+            return Response({'error': 'The product could not be found'}, status=status.HTTP_404_NOT_FOUND)
 
     order = Order.objects.create(
-        address=new_order['address'],
-        firstname=new_order['firstname'],
-        lastname=new_order['lastname'],
-        phonenumber=new_order['phonenumber']
+        address=new_order_address,
+        firstname=new_order_firstname,
+        lastname=new_order_lastname,
+        phonenumber=new_order_phonenumber
     )
 
-    for new_order_product in new_order['products']:
+    for new_order_product in new_order_products:
         product = Product.objects.get(pk=new_order_product['product'])
 
         order_product = OrderProduct(order=order)
